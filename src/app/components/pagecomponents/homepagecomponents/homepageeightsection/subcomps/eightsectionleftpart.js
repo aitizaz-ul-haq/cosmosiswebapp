0b8d24dee08286ad52500b4c691f1f9dcae9b501
aitizaz-ul-haq <atz.softprgmr@gmail.com>
2025-10-02 15:48:29 +0500

    added changes

--- src/app/components/pagecomponents/homepagecomponents/homepageeightsection/subcomps/eightsectionleftpart.js
+++ src/app/components/pagecomponents/homepagecomponents/homepageeightsection/subcomps/eightsectionleftpart.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import EightSectionLeftPartBottomSectionDemoSection from "./eightsectionleftpartbottomsectiondemocontaier";
 import EightSectionLeftPartTopSection from "./eightsectionleftparttopsection";
 
 export default function EightSectionLeftPart({
@@ -15,31 +15,10 @@ export default function EightSectionLeftPart({
         eightsectionleftsectionheading={eightsectionleftsectionheading}
         eightsectiontopline={eightsectiontopline}
       />
-      <div className="left-part-bottomsection-demo-container">
-        <div className="left-part-bottomsection-heading">
-          {eightsectionbottomheading}
-        </div>
-
-        {eightsectionleftsectionbottomlines.map((line) => (
-          <div
-            className="left-part-bottomsection-instructions-container"
-            key={line.id}
-          >
-            <div className="left-part-bottomsection-line">
-              <Image
-                src={line.iconlink}
-                width={line.width}
-                height={line.height}
-                alt={line.alt}
-                title={line.title}
-              />
-              <p className="left-part-bottomsection-statement">
-                {line.demoline}
-              </p>
-            </div>
-          </div>
-        ))}
-      </div>
+      <EightSectionLeftPartBottomSectionDemoSection
+        eightsectionbottomheading={eightsectionbottomheading}
+        eightsectionleftsectionbottomlines={eightsectionleftsectionbottomlines}
+      />
     </div>
   );
 }
---
+++ src/app/components/pagecomponents/homepagecomponents/homepageeightsection/subcomps/eightsectionleftpartbottomsectiondemocontaier.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+export default function EightSectionLeftPartBottomSectionDemoSection({
+  eightsectionbottomheading,
+  eightsectionleftsectionbottomlines,
+}) {
+  return (
+    <div className="left-part-bottomsection-demo-container">
+      <div className="left-part-bottomsection-heading">
+        {eightsectionbottomheading}
+      </div>
+      {eightsectionleftsectionbottomlines.map((line) => (
+        <div
+          className="left-part-bottomsection-instructions-container"
+          key={line.id}
+        >
+          <div className="left-part-bottomsection-line">
+            <Image
+              src={line.iconlink}
+              width={line.width}
+              height={line.height}
+              alt={line.alt}
+              title={line.title}
+            />
+            <p className="left-part-bottomsection-statement">{line.demoline}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
